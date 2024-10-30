90dd50adeaa3b93421ec22319176b29514ce0abc
Justineneema <j.neema@alustudent.com>
2024-10-30 21:36:28 +0200

    print and count

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-def safe_print_integer(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        print("{:d}".format(x))
-        count +=1
-        return True
-    except Exception:
-        return False
-    finally:
-        print()
-    return count      
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
+            raise
+    
+    print()
+    return count
