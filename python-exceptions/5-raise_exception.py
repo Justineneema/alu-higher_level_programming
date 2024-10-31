e7ab04abb499b19b5db533f499c5a7c7c5729cd2
Justineneema <j.neema@alustudent.com>
2024-10-31 12:07:34 +0200

    raise

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        raise = IndexError
-    result = 0
-    expect Exception as e:
-        print("you entered incorrect number")
-    finally:
-        print("run anyways")
-    return result
+    raise TypeError
