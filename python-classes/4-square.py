d83855e64beb7076cb92a2579e5397068b631020
Justineneema <j.neema@alustudent.com>
2024-11-04 13:47:08 +0200

    update

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,12 +20,12 @@ class Square:
 
     @property
     def size(self):
-       return self.__size
+        return self.__size
 
     @size.setter
     def size(self, size):
-       if type(size) is not int:
-          raise TypeError("size must be an integer")
-       if size <0 :
-          raise ValueError("size muzt be >+ 0")
-       self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
