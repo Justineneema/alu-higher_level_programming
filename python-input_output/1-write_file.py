38b7c5912e3bc5ea33d7b9d1b6d5396f9a808004
Justineneema <j.neema@alustudent.com>
2024-11-12 11:24:26 +0200

    writing in  a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Write a file and print it to stdout."""
+"""Write a string to a text file."""
 
 
 def write_file(filename="", text=""):
-    """Write a file and ptint it to stdout."""
+    """Write a string to a text file."""
     with open(filename, "w") as f:
-        return f.writel(text)
+        return f.write(text)
