3bd61506b63530fd0bb3bbee4684f468e32c08ff
Justineneema <j.neema@alustudent.com>
2024-11-12 12:53:16 +0200

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""a script that adds all arguments to a Python list, and then save them to a file:"""
-
+"""
+a script that adds all arguments to a Python list,
+and then save them to a file.
+"""
 
 import sys
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
-try:
-    items = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    items = []
+def add_items_to_json_file():
+    """a script that adds all arguments to a Python list."""
+    try:
+        current_items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        current_items = []
 
-items.extend(sys.argv[1:])
-save_to_json_file(items, "add_item.json")
+    current_items.extend(sys.argv[1:])
+
+    save_to_json_file(current_items, "add_item.json")
+
+if __name__ == "__main__":
+    add_items_to_json_file()
