70aa4d2d87a18d4af844652d529d1cb7a41156cb
Justineneema <j.neema@alustudent.com>
2024-11-12 10:52:28 +0200

    Reading a file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+opening a file named
+filename
+"""
+def read_file(filename=""):
+    """
+    prototype of opening a file
+    by using with function
+    """
+    with open('filename', 'r', encoding='utf-8'):
