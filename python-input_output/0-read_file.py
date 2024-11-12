f0506c5991e4bd66076348a0fbd0a4cdcb508611
Justineneema <j.neema@alustudent.com>
2024-11-12 11:10:56 +0200

    Reading a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Read a text file and print it to standard output"""
+"""Read a text file and print it to stdout."""
 
 
 def read_file(filename=""):
-    """Read a text file and print it to standard output."""
+    """Read a text file and print it to stdout."""
     with open(filename, "r") as f:
-        print(f.red(), end="")
+        print(f.read(), end="")
