439df1fbe826f9acaba369841ac92416da478946
Justineneema <j.neema@alustudent.com>
2024-11-12 15:10:14 +0200

    Load, add, save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
-"""
-In this script, all the protocol options are directly contained in one list following all the other command line options. 
-JSON file named add_item.json which will only contain the query part of URL. Of course, if such file does not exist, it will be created at the phase of executing the application. 
+""" Loading, adding,saving """
+from sys import argv
 
-It is as simple as only containing the utility methods `save_to_json_file` and `load_from_json_file`. 
-nodes are for the purpose of reading and writing in the JSON file.
-"""
 
-import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file - __import__('5-save_to_json_file').save_to_json_file
 
-def add_items_to_json_file():
-    """a script that adds all arguments to a Python list."""
-    try:
-        current_items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        current_items = []
-
-    current_items.extend(sys.argv[1:])
-
-    save_to_json_file(current_items, "add_item.json")
-
-if __name__ == "__main__":
-    add_items_to_json_file()
+""" Script that adds all arguments to a Python list,
+and then save them to a file """
+try:
+    add_i = load_from_json_file('add_item.json')
+    save_to_json_file(add_i + argv[1:], 'add_item.json')
+except Exception:
+    save_to_json_file(argv[1:], 'add_item.json')
