2f8987b29ff8d66be011b209c1cb08ed02a15f64
Justineneema <j.neema@alustudent.com>
2025-01-24 14:54:48 +0200

    Java wramup

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
-function factorial (n) {
-    return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+function factorial(n) {
+    if (isNaN(n) || n === 0) {
+      return 1; 
+    }
+    return n * factorial(n - 1);
   }
   
-  console.log(factorial(Number(process.argv[2])));
+  const input = Number(process.argv[2]);
+  console.log(factorial(input));
   
