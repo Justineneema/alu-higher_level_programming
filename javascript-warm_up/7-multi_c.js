4e0915979f2e9a4faa805e31099971f49f2fa096
Justineneema <j.neema@alustudent.com>
2025-01-24 11:11:38 +0200

    I LOve C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-if (Number.isNaN(Number(ProcessingInstruction.argv[2]))) {
+if (Number.isNaN(Number(process.argv[2]))) {
     console.log('Missing number of occurrences');
-} else {
-    const times = Number(ProcessingInstruction.argv[2].toFixed(0);
+  } else {
+    const times = Number(process.argv[2]).toFixed(0);
     for (let t = 0; t < times; t++) {
-        console.log('C is fun')
+      console.log('C is fun');
     }
-}
+  }
+  
