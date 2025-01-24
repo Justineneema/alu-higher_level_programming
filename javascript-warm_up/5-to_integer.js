08e38a51f17de3540a2055db9480602fa1140e2b
Justineneema <j.neema@alustudent.com>
2025-01-24 09:27:00 +0200

    An Integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (Number.isNaN(Number(Process.argv[2]))) {
+if (Number.isNaN(Number(process.argv[2]))) {
     console.log('Not a number');
-} else {
-    console.log(`My number: ${Number(Process.argv[2].toFixed(0))}`);
-}
+  } else {
+    console.log(`My number: ${Number(process.argv[2]).toFixed(0)}`);
+  }
