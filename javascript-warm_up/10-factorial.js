3f706498682fcc3c12a2a9e865c3b0537ae9e0af
Justineneema <j.neema@alustudent.com>
2025-01-24 15:46:46 +0200

    javascript-warm_up

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 function factorial (n) {
-    if (n === 1 || isNaN(n)) {
-      return 1;
-    }
-  
-    return n * factorial(n - 1);
-  }
-  
-  console.log(factorial(parseInt(process.argv[2])));
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+}
+
+console.log(factorial(Number(process.argv[2])));
