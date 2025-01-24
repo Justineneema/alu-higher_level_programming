2c962c2c998cf9265d47ce7d0ca605c6cb849418
Justineneema <j.neema@alustudent.com>
2025-01-24 08:04:52 +0200

    Arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const argv = Process.argv.slice(2);
+const argv = process.argv.slice(2);
 const message =
-    argv.length > 0
-        ? argv.length === 1
-            ? 'Argument found'
-            : 'Arguments found'
-        : 'No argument';
+  argv.length > 0
+    ? argv.length === 1
+      ? 'Argument found'
+      : 'Arguments found'
+    : 'No argument';
 console.log(message);
