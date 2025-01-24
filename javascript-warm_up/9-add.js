f12abefe73d43254f217f966dfad9bf4599ed002
Justineneema <j.neema@alustudent.com>
2025-01-24 15:56:42 +0200

    javascript-warm_up

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-function add (a, b) {
+function add(a, b) {
     return a + b;
   }
   
   const n1 = Number(process.argv[2]);
   const n2 = Number(process.argv[3]);
+  
   console.log(add(n1, n2));
+  
