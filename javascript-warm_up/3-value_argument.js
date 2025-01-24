04e2ae366316c11c0a341cfb2645651e067d40d8
Justineneema <j.neema@alustudent.com>
2025-01-24 08:59:07 +0200

    value my arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv[2]);
-  }
-  
+const argv = process.argv.slice(2);
+const message = argv[0] ? argv[0] : 'No argument';
+console.log(message);
