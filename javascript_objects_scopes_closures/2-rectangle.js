4c6068e78e75e4e9f5e6230fbd2ed4be02661a3a
Justineneema <j.neema@alustudent.com>
2025-01-28 10:58:50 +0200

    JavaScript - Objects, Scopes and Closures

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -2,15 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if(Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-        this.width = w;
-        this.height = h;
-        } else {
-    
-          return {};
-        }
-          
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
-Module.export = Rectangle;
+module.exports = Rectangle;
