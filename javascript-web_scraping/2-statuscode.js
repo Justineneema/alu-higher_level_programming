76e3c260326412cfde006236366180daea670fc1
Justineneema <j.neema@alustudent.com>
2025-02-05 10:32:49 +0200

    JavaScript - Web scraping

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -2,9 +2,9 @@
 const request = require('request');
 
 request(process.argv[2], function (err, res, body) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(`code: ${res.statuscode}`);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`code: ${res.statusCode}`);
+  }
 });
